65f64171180574d507eddc3ad1727d6b38dfb97b
markoz <markdrew53@gmail.com>
2020-05-18 23:30:12 -0500

    Injecting global styles

---
+++ pages/_app.tsx
@@ -0,0 +1,30 @@
+// Packages
+import { AppProps } from 'next/app';
+import Router from 'next/router';
+import nprogress from 'nprogress';
+import debounce from 'lodash.debounce';
+
+// Styles
+import 'react-notion/src/styles.css';
+import 'prismjs/themes/prism-tomorrow.css';
+import '@styles/global.css';
+
+// Only show nprogress after 500ms (slow loading)
+const start = debounce(nprogress.start, 500);
+
+Router.events.on('routeChangeStart', start);
+Router.events.on('routeChangeComplete', (url) => {
+  start.cancel();
+  nprogress.done();
+  window.scrollTo(0, 0);
+});
+Router.events.on('routeChangeError', () => {
+  start.cancel();
+  nprogress.done();
+});
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;
