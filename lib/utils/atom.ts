2db7bfce33e07314464c191966e1a81c3f08a8ca
Markoz Pena <markdrew53@gmail.com>
2021-04-04 11:08:58 -0500

    Create function for generating RSS the site

---
+++ lib/utils/atom.ts
@@ -0,0 +1,33 @@
+// Native
+import fs from 'fs/promises';
+
+// Packages
+import RSS from 'rss';
+
+// Data
+import * as data from '@data/blog.json';
+
+async function generateRSS() {
+  const feed = new RSS({
+    title: 'Markoz Peña',
+    site_url: 'https://markozxuu.now.sh',
+    feed_url: 'https://markozxuu.now.sh/atom.xml',
+  });
+
+  const onlyPublishedPosts = data
+    .filter((post) => post.published === true)
+    .reverse();
+  onlyPublishedPosts.map((post) => {
+    const { title, description, date, slug, author } = post;
+    feed.item({
+      title,
+      description,
+      date,
+      url: `/blog/${slug}`,
+      author,
+    });
+  });
+  await fs.writeFile('./public/atom.xml', feed.xml({ indent: true }));
+}
+
+export { generateRSS };
