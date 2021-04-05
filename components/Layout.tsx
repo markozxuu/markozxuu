343bda43a4ecb9ab0cff05b5e3657807235b66b1
Markoz Pena <markdrew53@gmail.com>
2021-04-04 22:58:59 -0500

    Add footer component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 // Components
-import Header from './Header';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 interface Props {
   children: React.ReactNode;
@@ -7,10 +8,15 @@ interface Props {
 
 const Layout = ({ children }: Props) => {
   return (
-    <div className="container mx-auto">
-      <Header />
-      <div>{children}</div>
-    </div>
+    <>
+      <div className="min-h-screen container mx-auto">
+        <Header />
+        <div>{children}</div>
+      </div>
+      <div className="my-10">
+        <Footer />
+      </div>
+    </>
   );
 };
 
