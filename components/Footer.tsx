684f66a79b8591ea7f3455b4331039dd908ab149
Markoz Pena <markdrew53@gmail.com>
2021-04-05 18:44:57 -0500

    Improves footer component style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,20 @@
 const Footer = () => {
   return (
     <footer className="px-2">
-      <span className="capitalize text-sm font-medium">©Markoz peña</span>
-      <a href="/atom.xml" className="underline text-sm font-medium">
-        RSS
-      </a>
+      <hr className="w-full border-t border-accent-3 dark:border-accent-2 mb-8" />
+      <div className="flex justify-between">
+        <span className="capitalize text-sm font-medium">©Markoz peña</span>
+        <a href="/atom.xml" className="underline text-sm font-medium block">
+          RSS
+        </a>
+      </div>
       <style jsx>{`
         @media (min-width: 500px) {
           footer {
             max-width: 42rem;
             margin: auto;
             padding: 20px;
-            display: flex;
           }
-        }
-        footer {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
         }
       `}</style>
     </footer>
