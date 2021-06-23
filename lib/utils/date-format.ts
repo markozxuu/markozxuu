2a283f7ca3db709676beaea66f0dcb62abfd2934
Markoz Pena <markdrew53@gmail.com>
2021-06-22 21:15:01 -0500

    refactor: Remove bad comments of TypeScript

--- lib/utils/date-format.ts
+++ lib/utils/date-format.ts
@@ -1,9 +1,5 @@
 function dateFormat(date: string) {
   const datePost = new Intl.DateTimeFormat('en-US', {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-nocheck
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
     dateStyle: 'medium',
   })
     .format(new Date(date))
