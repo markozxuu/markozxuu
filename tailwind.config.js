b75f439dfa575b8789182937c0cbc36aa83cfdda
Markoz Pena <markdrew53@gmail.com>
2021-07-05 17:07:40 -0500

    feat: Added `@tailwindcss/line-clamp` plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,5 +42,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/line-clamp')],
 };
