1e0c5025cceaa46827cd4db5205173cbc1705f6b
Markoz Pena <markdrew53@gmail.com>
2021-07-05 16:05:59 -0500

    feat: Create `<Footer>` component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,46 @@
+import s from './footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={s.root}>
+      <div className={s.container}>
+        <div className="pb-8 mb-2 border-t-2 border-accent-3 dark:border-accent-2" />
+        <div className="flex flex-col justify-between lg:flex-row">
+          <p>Thanks for reading! :)</p>
+          <div className="pt-2 space-x-6 font-medium lg:pt-0">
+            <a
+              href="https://twitter.com/markozxuu"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+            <a
+              href="https://github.com/markozxuu"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://www.linkedin.com/in/markozpena/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Linkedin
+            </a>
+            <a
+              href="https://www.markozxuu.com/atom.xml"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              RSS
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
