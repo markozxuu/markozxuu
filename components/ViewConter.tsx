09a2a5faf29b9b132331b1981cb9198a56bb51e1
Markoz Pena <markdrew53@gmail.com>
2021-09-17 01:40:57 -0500

    feat: Change `swr` to `useSwr`

--- components/ViewConter.tsx
+++ components/ViewConter.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import swr from 'swr';
+import useSWR from 'swr';
 
 import fetcher from '@lib/fetcher';
 
@@ -10,7 +10,7 @@ interface Props {
 }
 
 const ViewCounter = ({ slug }: Props) => {
-  const { data } = swr(`/api/views/${slug}`, fetcher);
+  const { data } = useSWR(`/api/views/${slug}`, fetcher);
   const views = data?.total;
   useEffect(() => {
     const registerView = () => {
