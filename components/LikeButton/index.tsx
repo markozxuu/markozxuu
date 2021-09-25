3b7d093083904ac867d9776295170fa350a332c9
Markoz Pena <markdrew53@gmail.com>
2021-09-25 15:44:41 -0500

    feat: Enable sound to `<LikeButton>`

--- components/LikeButton/index.tsx
+++ components/LikeButton/index.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import useSound from 'use-sound';
 import cx from 'clsx';
 
+import { LIMIT_LIKES } from '@lib/utils/const';
 import { useSettings } from '@lib/useSettings';
 import { usePostLike } from '@lib/usePostLikes';
 
@@ -14,8 +18,29 @@ interface LikeButtonProps {
 }
 
 const LikeButton = ({ slug }: LikeButtonProps) => {
+  const GLUG_SOUND = '/sounds/glug-a.mp3';
+
+  const [playbackRate, setPlaybackRate] = useState(0.75);
+  const [glugSound, { stop: glugOff }] = useSound(GLUG_SOUND, {
+    playbackRate,
+    volume: 0.5,
+  });
   const { isSound } = useSettings();
   const { likesUser, totalLikesPost, increment, isLoading } = usePostLike(slug);
+
+  const handleClick = () => {
+    if (!isSound) {
+      return glugOff();
+    }
+
+    if (likesUser >= LIMIT_LIKES) {
+      return glugOff();
+    }
+
+    setPlaybackRate(playbackRate + 0.1);
+    glugSound();
+    increment();
+  };
 
   if (isLoading) {
     return (
@@ -30,8 +55,21 @@ const LikeButton = ({ slug }: LikeButtonProps) => {
 
   return (
     <div className={s.root}>
-      <h1>{isSound ? 'true' : 'false'}</h1>
-      <button aria-label="Like button" onClick={increment}>
+      <motion.div
+        className="absolute w-full text-2xl text-center"
+        animate={likesUser >= LIMIT_LIKES ? 'visible' : 'hidden'}
+        variants={{
+          hidden: { translateY: -80, opacity: 0 },
+          visible: {
+            translateY: [0, -50, -60],
+            opacity: [0, 1, 0],
+          },
+        }}
+        initial="hidden"
+      >
+        🎉
+      </motion.div>
+      <button aria-label="Like button" onClick={handleClick}>
         <Heart likesUser={String(likesUser)} />
       </button>
 
