49aa18130d93a3fc4fd01ffb1752044fdc284951
Markoz Pena <markdrew53@gmail.com>
2023-08-13 19:15:39 -0600

    refactor: remove unnecessary code

--- pages/api/clear-preview.ts
+++
@@ -1,9 +0,0 @@
-// Packages
-import { NextApiRequest, NextApiResponse } from 'next';
-
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.clearPreviewData();
-  return res.redirect('/blog');
-}
-
-export default handler;
