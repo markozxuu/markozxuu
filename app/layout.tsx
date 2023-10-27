9702c3ec8e7f652118bf6067d3b1b40d1cc69d8c
Markoz Pena <markdrew53@gmail.com>
2023-10-27 14:57:11 -0600

    feat: improve layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 
-import { Header } from '@components/commons/Header';
+import Header from '@components/commons/Header';
 
 import '../styles/global.css';
 
