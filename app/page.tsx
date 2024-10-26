cc5a7b18fba8c0fa169fac0e342d1f8fc6aff640
Markoz Peña <markdrew53@gmail.com>
2024-10-26 14:36:46 -0600

    sort imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Card from './ui/home/card'
 import React from 'react'
+import Card from './ui/home/card'
 
 function IndexPage() {
   let a = 1
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello Worlwwwd</h1>
       <Card />
     </div>
   )
