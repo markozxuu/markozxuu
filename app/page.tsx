26a4f7e7af050a704db256479c09f450d1b211de
Markoz Peña <markdrew53@gmail.com>
2024-10-26 13:40:09 -0600

    feat: create index file page

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './ui/home/card'
+
+function IndexPage() {
+  let a = 1
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Card />
+    </div>
+  )
+}
+
+export default IndexPage
---
+++ app/ui/home/card/Card.tsx
@@ -0,0 +1,9 @@
+function Card() {
+  return (
+    <div>
+      <h1>Card</h1>
+    </div>
+  )
+}
+
+export default Card
