583b4669ef6678bb43a9d39d17af451a6505b4eb
Markoz Peña <markdrew53@gmail.com>
2024-10-27 14:01:32 -0600

    feat: improvin the styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Card from './ui/home/card'
 function IndexPage() {
   return (
     <div>
-      <h1 className="font-semibold text-4xl text-teal-600">Hello Mark</h1>
+      <h1 className="text-3xl font-bold underline">Hello Mark</h1>
       <Card />
     </div>
   )
