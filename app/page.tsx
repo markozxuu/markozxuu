d35a43ad8fc9411a48f98ea007dd29517d57659a
Markoz Peña <markdrew53@gmail.com>
2024-10-27 13:51:30 -0600

    feat: improving the styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Card from './ui/home/card'
 function IndexPage() {
   return (
     <div>
-      <h1>Hellssso</h1>
+      <h1 className="font-semibold text-4xl text-teal-600">Hello Mark</h1>
       <Card />
     </div>
   )
