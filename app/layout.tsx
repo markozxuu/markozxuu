5e94bc55fb954567a68f9be2345b2eeefbaba494
Markoz Peña <markdrew53@gmail.com>
2024-10-27 14:05:33 -0600

    feat: create metadata for the root page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
+import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
+
+// Import global styles
 import './css/globals.css'
+
+export const metadata: Metadata = {
+  title: 'My App :D',
+  description: 'My app description',
+}
 
 function Layout({ children }: { children: ReactNode }) {
   return (
