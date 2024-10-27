1548ab65fcf4893e2bbf7406760896715dd89870
Markoz Peña <markdrew53@gmail.com>
2024-10-27 13:27:13 -0600

    configt: first test 2

--- app/ui/home/card/Card.tsx
+++ app/ui/home/card/Card.tsx
@@ -1,10 +1,11 @@
 'use client'
+import Image from 'next/image'
 
 function Card() {
   return (
     <div>
-      <h1>Card kkskskskskk</h1>
-      <img src="https://via.placeholder.com/150" alt="placeholder" />
+      <h1>Card ssss</h1>
+      <Image src="https://via.placeholder.com/150" alt="placeholder" />
     </div>
   )
 }
