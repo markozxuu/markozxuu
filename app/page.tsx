eed9cc2d5f30e760bff603c9ed11efc75418bbb6
Markoz Peña <markdrew53@gmail.com>
2024-10-31 11:39:47 -0600

    feat: aplpy code

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Card from './ui/home/card'
 function IndexPage() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello Mark</h1>
+      <h1 className="text-3xl font-bold underline">Portafolio</h1>
       <Card />
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{tsx,mdx}'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
