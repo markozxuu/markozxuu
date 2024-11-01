0b6a69d17d85e6f3fa41da3f4048b1605f77aaa8
Markoz Peña <markdrew53@gmail.com>
2024-10-31 18:24:35 -0600

    feat: sksksk

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Card from './ui/home/card'
 
 function IndexPage() {
-                      const name = 'Jhon Doe'
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Portafolio</h1>
