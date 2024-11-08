118bd909653ce7ae332337a99cee971f5012afb0
Markoz Peña <markdrew53@gmail.com>
2024-11-08 11:32:11 -0600

    refactor: remove a element

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,20 +1,12 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
 const Header = () => {
   return (
     <header className="fles justify-between">
-      <Image src="/logo.png" alt="logo" className="w-12 h-12" />
       <nav>
-        <Link href="">
-          <a>Home</a>
-        </Link>
-        <Link href="">
-          <a>Projects</a>
-        </Link>
-        <Link href="">
-          <a>About</a>
-        </Link>
+        <Link href="">Home</Link>
+        <Link href="">Projects</Link>
+        <Link href="">About</Link>
       </nav>
     </header>
   )
