b5b8ec2bbe3c7cec9805e82d291802c07457b18d
Markoz Peña <markdrew53@gmail.com>
2024-11-08 09:55:46 -0600

    refactor: change the title

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Card from './ui/home/card'
 
 function IndexPage() {
   return (
-    <div>
+    <main>
       <h1 className="text-3xl font-bold underline">Portafolio</h1>
-      <Card />
-    </div>
+    </main>
   )
 }
 
