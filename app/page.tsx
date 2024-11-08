d58b308537a5762895b2878d0d91669a9e35c54c
Markoz Peña <markdrew53@gmail.com>
2024-11-08 11:32:33 -0600

    feat: change color the title

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function IndexPage() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Portafolio</h1>
+      <h1 className="text-3xl font-bold underline text-blue-50">Portafolio</h1>
     </main>
   )
 }
