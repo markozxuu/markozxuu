8525492eea4ff39c0aef8cae40b62d533e6fc284
Markoz Peña <markdrew53@gmail.com>
2024-11-17 14:57:21 -0600

    feat: create avatar image

--- app/ui/header/header.tsx
+++ app/ui/header/header.tsx
@@ -1,9 +1,17 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 const Header = () => {
   return (
-    <header className="fles justify-between">
-      <nav>
+    <header className="flex justify-between items-center py-3 w-[1120px]">
+      <Image
+        className="rounded-full w-12 h-12"
+        src="/images/me.jpg"
+        width={48}
+        height={48}
+        alt="Logo"
+      />
+      <nav className="flex flex-row gap-x-10 opacity-80">
         <Link href="">Home</Link>
         <Link href="">Projects</Link>
         <Link href="">About</Link>
