794a4e1a2d8c64c42f81401f3e6b76fe2a765d45
Markoz Peña <markdrew53@gmail.com>
2024-11-30 18:48:06 -0600

    feat(home): create styles for the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Badge from './ui/badge/badge'
 
 function IndexPage() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline text-blue-50">Portafolio</h1>
+      <section className="w-full mx-auto lg:w-[740px] py-44">
+        <h1 className="text-5xl font-bold flex justify-center flex-row gap-x-4 pb-4">
+          Hey, soy Markoz
+          <div className="flex justify-center items-center">
+            <Badge>No disponible para trabajar</Badge>
+          </div>
+        </h1>
+        <h2 className="text-xl opacity-80">
+          +5 años de experiencia. Desarollador Web. De Veracruz, México 🇲🇽.
+          Especializado en crear aplicaciones altamente escalables.
+        </h2>
+
+        <ul>
+          <li>markoz</li>
+        </ul>
+      </section>
     </main>
   )
 }
