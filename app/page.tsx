6b72f41d7a8ca594db82083ef2684d4262db008e
Markoz Peña <markdrew53@gmail.com>
2024-12-05 21:07:17 -0600

    feat: adjusint the home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 import Badge from './ui/badge/badge'
 
@@ -5,11 +6,23 @@ function IndexPage() {
   return (
     <main>
       <section className="w-full mx-auto lg:w-[740px] py-44">
-        <h1 className="text-5xl font-bold flex justify-center flex-row gap-x-4 pb-4">
+        <Image
+          className="rounded-full w-12 h-12 mb-4"
+          src="/images/me.jpg"
+          width={48}
+          height={48}
+          alt="Logo"
+        />
+        <h1 className="text-5xl font-bold flex flex-row gap-x-4 pb-10">
           Hey, soy Markoz
-          <div className="flex justify-center items-center">
+          <a
+            href="https://www.linkedin.com/in/markozpena/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex justify-center items-center"
+          >
             <Badge>No disponible para trabajar</Badge>
-          </div>
+          </a>
         </h1>
         <h2 className="text-xl opacity-80">
           +5 años de experiencia. Desarollador Web. De Veracruz, México 🇲🇽.
