18e0588e317d69a26d8897ce1143f5926a627a1a
Markoz Peña <markdrew53@gmail.com>
2024-12-05 23:11:45 -0600

    feat: add gradient to the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,12 @@ function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>
-        <Header />
-        {children}
+        <div className="relative h-full w-full bg-slate-950">
+          <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+          <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+          <Header />
+          {children}
+        </div>
       </body>
     </html>
   )
