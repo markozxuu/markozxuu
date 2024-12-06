63eced5b932c9e699eb620811b0f76eea064d2d4
Markoz Peña <markdrew53@gmail.com>
2024-12-05 21:07:31 -0600

    feat: adjuting the badge component

--- app/ui/badge/badge.tsx
+++ app/ui/badge/badge.tsx
@@ -6,7 +6,7 @@ interface BadgeProps {
 
 const Badge: FC<BadgeProps> = ({ children }) => {
   return (
-    <span className=" bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+    <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
       {children}
     </span>
   )
