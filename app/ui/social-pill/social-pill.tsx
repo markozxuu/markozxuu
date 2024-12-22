dd6a2e5b5e8502b1dfecad1c81ee1bbb12ccc199
Markoz Peña <markdrew53@gmail.com>
2024-12-21 19:26:15 -0600

    feat: create SocialPill component

---
+++ app/ui/social-pill/social-pill.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react'
+
+const SocialPill: FC = () => {
+  return (
+    <div className="flex items-center gap-2 bg-gray-100/50 rounded-lg p-2">
+      <p>jskks</p>
+    </div>
+  )
+}
+
+export default SocialPill
