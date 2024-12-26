f1dd971a232654e5ef173d65aef11259e88d7210
Markoz Peña <markdrew53@gmail.com>
2024-12-25 22:51:01 -0600

    styles: set the overflow as auto

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>
-        <div className="relative h-full w-full bg-slate-950 overflow-hidden">
+        <div className="relative h-full w-full bg-slate-950 overflow-auto">
           <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
           <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
           <div className="relative z-10">
