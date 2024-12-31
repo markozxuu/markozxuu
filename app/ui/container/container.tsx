0ce1444a1a575585fd41e0435ae85163d0400c80
Markoz Peña <markdrew53@gmail.com>
2024-12-31 16:40:16 -0600

    feature: add id prop

--- app/ui/container/container.tsx
+++ app/ui/container/container.tsx
@@ -3,10 +3,11 @@ import type { FC, ReactNode } from 'react'
 interface ContainerProps {
   children: ReactNode
   className?: string
+  id?: string
 }
 
-const Container: FC<ContainerProps> = ({ children, className }) => (
-  <section className={`w-full mx-auto lg:w-[740px] ${className}`}>
+const Container: FC<ContainerProps> = ({ children, className, id }) => (
+  <section id={id} className={`w-full mx-auto lg:w-[740px] ${className}`}>
     {children}
   </section>
 )
