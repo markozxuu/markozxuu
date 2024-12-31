582f9ef083d3372d938e02f6eb6d7ddd66486298
Markoz Peña <markdrew53@gmail.com>
2024-12-31 15:26:18 -0600

    feature: remove all property the waring SVG

--- app/ui/experience-item/experience-item.tsx
+++ app/ui/experience-item/experience-item.tsx
@@ -39,9 +39,9 @@ const ExperienceItem: FC<ExperienceItemProps> = ({
         >
           <path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M1 5h12m0 0L9 1m4 4L9 9"
           />
         </svg>
