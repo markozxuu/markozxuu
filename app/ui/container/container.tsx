b97cd02050969f743c6df21f5be999e34351f558
Markoz Peña <markdrew53@gmail.com>
2024-12-31 12:47:00 -0600

    styles: add styles dynamic

--- app/ui/container/container.tsx
+++ app/ui/container/container.tsx
@@ -2,10 +2,13 @@ import type { FC, ReactNode } from 'react'
 
 interface ContainerProps {
   children: ReactNode
+  className?: string
 }
 
-const Container: FC<ContainerProps> = ({ children }) => (
-  <section className="w-full mx-auto lg:w-[740px] py-44">{children}</section>
+const Container: FC<ContainerProps> = ({ children, className }) => (
+  <section className={`w-full mx-auto lg:w-[740px] ${className}`}>
+    {children}
+  </section>
 )
 
 export default Container
