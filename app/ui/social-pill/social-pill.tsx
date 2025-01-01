df127f02f14b0f3668bec410407b8c61f5f9074b
Markoz Peña <markdrew53@gmail.com>
2025-01-01 11:27:15 -0600

    styles: add support to mobile

--- app/ui/social-pill/social-pill.tsx
+++ app/ui/social-pill/social-pill.tsx
@@ -11,7 +11,7 @@ const SocialPill: FC<SocialPillProps> = ({ children, link }) => {
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="rounded-full border border-white/10 flex justify-center items-center gap-x-2 py-2 px-4 bg-white/5 hover:scale-110 hover:bg-white/10 transition"
+      className="rounded-full border border-white/10 flex justify-center items-center gap-x-2 py-1 px-2 md:py-2 md:px-4 text-xs md:text-base bg-white/5 hover:scale-110 hover:bg-white/10 transition"
     >
       {children}
     </a>
