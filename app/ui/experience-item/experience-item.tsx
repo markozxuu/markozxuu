96a33b56a4227f1f92f3e143a7591f153b0caab6
Markoz Peña <markdrew53@gmail.com>
2025-01-01 11:29:13 -0600

    styles: add new color to the date

--- app/ui/experience-item/experience-item.tsx
+++ app/ui/experience-item/experience-item.tsx
@@ -15,7 +15,7 @@ const ExperienceItem: FC<ExperienceItemProps> = ({
 }) => (
   <>
     <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+    <time className="mb-1 text-sm font-normal leading-none text-red-400 ">
       {date}
     </time>
     <h3 className="text-lg font-semibold text-yellow-200">{title}</h3>
