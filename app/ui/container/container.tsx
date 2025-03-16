ecef5beaf6a925d7183cab9df73af8c05fb8eea3
Markoz Peña <markdrew53@gmail.com>
2025-03-16 00:17:02 -0600

    feat: improving the logic of the <Container> component

--- app/ui/container/container.tsx
+++ app/ui/container/container.tsx
@@ -1,17 +1,41 @@
-import clsx from 'clsx'
+import cx from 'clsx'
 import React, { ReactNode, forwardRef } from 'react'
 
-interface OuterContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
   children: ReactNode
   className?: string
 }
 
-export const OuterContainer = forwardRef<HTMLDivElement, OuterContainerProps>(
+export const OuterContainer = forwardRef<HTMLDivElement, ContainerProps>(
   ({ children, className, ...props }, ref) => {
     return (
-      <div ref={ref} className={clsx('sm:px-8', className)} {...props}>
+      <div ref={ref} className={cx('sm:px-8', className)} {...props}>
         <div className="mx-auto max-w-7xl lg:px-8">{children}</div>
       </div>
+    )
+  },
+)
+
+export const InnerContainer = forwardRef<HTMLDivElement, ContainerProps>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={cx('relative px-4 sm:px-8 lg:px-12', className)}
+        {...props}
+      >
+        <div className="mx-auto max-w-2xl lg:max-w-5xl">{children}</div>
+      </div>
+    )
+  },
+)
+
+export const Container = forwardRef<HTMLDivElement, ContainerProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <OuterContainer ref={ref} {...props}>
+        <InnerContainer>{children}</InnerContainer>
+      </OuterContainer>
     )
   },
 )
