4114c86843d93dec151fdde419b455e2988831ab
Markoz Peña <markdrew53@gmail.com>
2025-03-16 14:04:19 -0600

    feat: create navLinks for the navegation

--- app/ui/footer/footer.tsx
+++ app/ui/footer/footer.tsx
@@ -1,12 +1,25 @@
+// Components
+import { InnerContainer, OuterContainer } from '../container/container'
+import NavLink from '../nav-link/nav-link'
+
 const Footer = () => (
-  <footer className="mt-32 flex-none">
-    <div className="sm:px-8">
-      <div className="mx-auto w-full max-w-7xl lg:px-8">
-        <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
-          <p>sksk</p>
-        </div>
+  <footer className="mt-32">
+    <OuterContainer>
+      <div className="border-t border-zinc-100 dark:border-zinc-700/40 pt-10 pb-16">
+        <InnerContainer>
+          <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+            <div className="flex gap-6 font-medium text-sm">
+              <NavLink href="/about">About</NavLink>
+              <NavLink href="/blog">Blog</NavLink>
+              <NavLink href="/proyect">Projects</NavLink>
+            </div>
+            <p className="text-sm text-zinc-400 dark:text-zinc-500">
+              Markoz Peña (@markozxuu)
+            </p>
+          </div>
+        </InnerContainer>
       </div>
-    </div>
+    </OuterContainer>
   </footer>
 )
 
