6d21f8cb42231d0bf94d32095a142de4717e8643
Markoz Peña <markdrew53@gmail.com>
2025-03-22 08:08:08 -0600

    feat: enable viewTransition experimental API

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: {
+    viewTransition: true,
+  },
 }
 
 export default nextConfig
